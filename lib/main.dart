9c786c3e6d19a6559bd614888195c11228aa815a
patrick.whelan <patrick.whelan@telekom.de>
2024-10-24 11:04:42 +0200

    fix: update app license.

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ import 'package:arc_view/src/app.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:logging/logging.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logging/logging.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -29,6 +29,8 @@ Future<void> main() async {
   LicenseRegistry.addLicense(() async* {
     final license = await rootBundle.loadString('fonts/LICENSE.txt');
     yield LicenseEntryWithLineBreaks(['RobotoMono'], license);
+    final apache2 = await rootBundle.loadString('LICENSES/Apache-2.0.txt');
+    yield LicenseEntryWithLineBreaks(['Arc View'], apache2);
   });
 
   runApp(const ProviderScope(child: MessagingApp()));
