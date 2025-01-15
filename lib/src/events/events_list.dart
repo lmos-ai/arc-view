15e5a5610d5feaaa362f553a8aaf9eb885552eee
Dharmendra Singh <dharmendra.singh1291@gmail.com>
2025-01-15 18:35:05 +0530

    Initial Draft: Provide support of Event filtering and sorting (#10)
    * fix: fix event-subscriptions link.
    
    * Initial Draft: Provide support of Event filtering and sorting
    
    * Add Filtering and sorting on Events
    
    * Code Refactoring for change UI placeholder of filter & sort
    
    * Resolve review comment (#21)
    
    Co-authored-by: Dharmendra Singh <dharmendrasingh@dharmendra.singh>
    
    * Add Events count information (#11)
    
    * Add Events count information
    
    * Incorporate Review#Cmt (#15)
    
    Co-authored-by: Dharmendra Singh <dharmendrasingh@dharmendra.singh>
    
    ---------
    
    Co-authored-by: Dharmendra Singh <dharmendrasingh@dharmendra.singh>
    
    ---------
    
    Co-authored-by: patrick.whelan <patrick.whelan@telekom.de>
    Co-authored-by: Dharmendra Singh <dharmendrasingh@dharmendra.singh>
    Co-authored-by: Kai Kreuzer <kai.kreuzer@telekom.de>
    Co-authored-by: patwlan <163450632+patwlan@users.noreply.github.com>

--- lib/src/events/events_list.dart
+++ lib/src/events/events_list.dart
@@ -8,6 +8,7 @@ import 'dart:convert';
 
 import 'package:arc_view/src/core/text.dart';
 import 'package:arc_view/src/events/event_row_item.dart';
+import 'package:arc_view/src/events/models/agent_events.dart';
 import 'package:arc_view/src/events/notifiers/agent_events_notifier.dart';
 import 'package:arc_view/src/events/prompt_view.dart';
 import 'package:flutter/material.dart';
@@ -15,10 +16,22 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:smiles/smiles.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
-class EventsList extends ConsumerWidget {
-  EventsList({super.key});
+// State provider to manage the drawer state
+final filterDrawerProvider = StateProvider<bool>((ref) => false);
 
-  final _handleEvents = [
+// State provider to manage available filters
+final availableFiltersProvider = StateProvider<Map<String, List<String>>>(
+    (ref) => {'EventType': [], 'Conversation': [], 'AgentName': []});
+
+//State Provider for filter group
+final eventTypeFilterProvider = StateProvider<List<String>>((ref) => []);
+final conversationFilterProvider = StateProvider<List<String>>((ref) => []);
+final agentNameFilterProvider = StateProvider<List<String>>((ref) => []);
+
+class EventsList extends ConsumerWidget {
+  const EventsList({super.key});
+
+  static final handleEvents = [
     'AgentFinishedEvent',
     'LLMFinishedEvent',
     'LLMFunctionCalledEvent',
@@ -28,57 +41,75 @@ class EventsList extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // Watch the state providers
+    final isFilterDrawerOpen = ref.watch(filterDrawerProvider);
+
     final events = ref.watch(agentEventsNotifierProvider).where((e) {
-      return _handleEvents.contains(e.type);
+      return handleEvents.contains(e.type) && applyFiltersAndSorting(e, ref);
     }).toList();
 
-    return events.isEmpty
-        ? Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              'No events'.small,
-              SmallLinkedText(
-                'Click here for details',
-                tip: 'Open Help page in a browser',
-                onPressed: () {
-                  launchUrlString(
-                      'https://eclipse.dev/lmos/docs/arc/spring/graphql#event-subscriptions');
-                },
-              ),
-            ],
-          )
-        : ListView.builder(
-            padding: const EdgeInsets.all(16),
-            itemCount: events.length,
-            itemBuilder: (context, index) {
-              final event = events[index];
-              final json = jsonDecode(event.payload) as Map<String, dynamic>;
-              final contextLabel = _getEventLabel(json);
+    // Schedule the filter update after the widget tree has been built
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _updateFilterProvider(ref);
+    });
 
-              return Card(
-                elevation: 0,
-                child: ExpansionTile(
-                  expandedAlignment: Alignment.topLeft,
-                  childrenPadding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-                  title: EventRowItem(event: event, json: json),
-                  subtitle: [
-                    SmallText(contextLabel),
-                    Spacer(),
-                    SmallText(json['duration'] != null
-                        ? '${(json['duration'] as double?)?.toStringAsPrecision(3)} seconds'
-                        : ''),
-                  ].row(),
+    return Stack(alignment: Alignment.topLeft, children: [
+      Positioned.fill(
+          child: events.isEmpty
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children:
-                          _transformEvent(event.type, context, json).toList(),
+                    'No events'.small,
+                    SmallLinkedText(
+                      'Click here for details',
+                      tip: 'Open Help page in a browser',
+                      onPressed: () {
+                        launchUrlString(
+                            'https://eclipse.dev/lmos/docs/arc/spring/graphql#event-subscriptions');
+                      },
                     ),
                   ],
-                ),
-              );
-            },
-          );
+                )
+              : Column(children: [
+                  Expanded(
+                    child: ListView.builder(
+                      padding: const EdgeInsets.all(16),
+                      itemCount: events.length,
+                      itemBuilder: (context, index) {
+                        final event = events[index];
+                        final json =
+                            jsonDecode(event.payload) as Map<String, dynamic>;
+                        final contextLabel = _getEventLabel(json);
+
+                        return Card(
+                          elevation: 0,
+                          child: ExpansionTile(
+                            expandedAlignment: Alignment.topLeft,
+                            childrenPadding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                            title: EventRowItem(event: event, json: json),
+                            subtitle: [
+                              SmallText(contextLabel),
+                              Spacer(),
+                              SmallText(json['duration'] != null
+                                  ? '${(json['duration'] as double?)?.toStringAsPrecision(3)} seconds'
+                                  : ''),
+                            ].row(),
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children:
+                                    _transformEvent(event.type, context, json).toList(),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                ])),
+      //Custom Filter Drawer
+      if (isFilterDrawerOpen) _buildFilterDrawer(context, ref)
+    ]);
   }
 
   String _getEventLabel(Map<String, dynamic> json) {
@@ -140,5 +171,175 @@ class EventsList extends ConsumerWidget {
     } catch (e) {
       return '';
     }
+  }
+
+  static bool applyFiltersAndSorting(AgentEvent e, WidgetRef ref) {
+    final selectedEventTypes = ref.watch(eventTypeFilterProvider);
+    final selectedConversations = ref.watch(conversationFilterProvider);
+    final selectedAgents = ref.watch(agentNameFilterProvider);
+    bool matchesEventType =
+        selectedEventTypes.isEmpty || selectedEventTypes.contains(e.type);
+    bool matchesConversations = selectedConversations.isEmpty ||
+        selectedConversations.contains(e.conversationId);
+
+    //Agent is the part of payload to decode it
+    var agentName = json.decode(e.payload)['agent']?['name'] ?? '';
+    bool matchesAgentName =
+        selectedAgents.isEmpty || selectedAgents.contains(agentName);
+    //To-DO Sorting
+
+    return matchesEventType && matchesConversations && matchesAgentName;
+  }
+
+  //Dynamically Update Filter Group Values
+  _updateFilterProvider(WidgetRef ref) {
+    //Get All Supported Events
+    final events = ref
+        .watch(agentEventsNotifierProvider.select((list) => list.where((e) {
+              return handleEvents.contains(e.type);
+            })))
+        .toList();
+
+    // Extract unique values for each filter type
+    final eventTypes = events.map((e) => e.type).toSet().toList();
+    // Extract Conversations
+    final conversationNames =
+        events.map((e) => e.conversationId!!).toSet().toList();
+    //Extract Agent Name from payload
+    final List<String> agentNames = events
+        .map((e) {
+          // Decode the JSON payload and extract the agent name
+          var agentName = json.decode(e.payload)['agent']?['name'];
+          return agentName?.toString() ??
+              ''; // Return the name, which may be null
+        })
+        .where((name) => name.isNotEmpty) // Remove null values, if any
+        .toSet() // Ensure uniqueness
+        .toList();
+
+    // Update available filters (Dynamically Calculate)
+    ref.read(availableFiltersProvider.notifier).state = {
+      'EventType': eventTypes,
+      'Conversation': conversationNames,
+      'AgentName': agentNames,
+    };
+  }
+
+  //Create Custom Filter Drawer
+  _buildFilterDrawer(BuildContext context, WidgetRef ref) {
+    return Positioned(
+      top: 0,
+      left: 0,
+      bottom: 0,
+      width: 300,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        color: Theme.of(context).colorScheme.surface,
+        child: Column(
+          children: [
+            // Drawer Header
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 4.0, vertical: 4.0),
+              color: Theme.of(context).colorScheme.primary,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back, color: Colors.white),
+                    onPressed: () {
+                      ref.read(filterDrawerProvider.notifier).state = false;
+                    },
+                  ),
+                  Text(
+                    'Filters',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Clear all selected filters
+                      ref.read(eventTypeFilterProvider.notifier).state = [];
+                      ref.read(conversationFilterProvider.notifier).state = [];
+                      ref.read(agentNameFilterProvider.notifier).state = [];
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      elevation: 0,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16.0, vertical: 8.0),
+                    ),
+                    child: Icon(Icons.filter_alt_off),
+                  ),
+                ],
+              ),
+            ),
+            // Filter List
+            Expanded(
+              child: ListView(
+                children:
+                    ref.watch(availableFiltersProvider).entries.map((entry) {
+                  final filterType = entry.key;
+                  final options = entry.value;
+                  final selectedValues = ref.watch(
+                    filterType == 'EventType'
+                        ? eventTypeFilterProvider
+                        : filterType == 'Conversation'
+                            ? conversationFilterProvider
+                            : agentNameFilterProvider,
+                  );
+                  return _buildFilterGroup(
+                      filterType, selectedValues, options, ref);
+                }).toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFilterGroup(String filterType, List<String> selectedValues,
+      List<String> options, WidgetRef ref) {
+    return ExpansionTile(
+      title: Text(
+        filterType,
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      children: options.map((option) {
+        final isSelected = selectedValues.contains(option);
+
+        return CheckboxListTile(
+          title: Text(option),
+          value: isSelected,
+          onChanged: (bool? value) {
+            final updatedValues = List<String>.from(selectedValues);
+            if (value == true) {
+              updatedValues.add(option);
+            } else {
+              updatedValues.remove(option);
+            }
+
+            switch (filterType) {
+              case 'EventType':
+                ref.read(eventTypeFilterProvider.notifier).state =
+                    updatedValues;
+                break;
+              case 'Conversation':
+                ref.read(conversationFilterProvider.notifier).state =
+                    updatedValues;
+                break;
+              case 'AgentName':
+                ref.read(agentNameFilterProvider.notifier).state =
+                    updatedValues;
+                break;
+            }
+          },
+        );
+      }).toList(),
+    );
   }
 }
--- lib/src/events/events_panel.dart
+++ lib/src/events/events_panel.dart
@@ -12,11 +12,15 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:smiles/smiles.dart';
 
+// Provider for sorting state (ASC or DESC)
+final selectedSortProvider = StateProvider<String>((ref) => 'DESC');
+
 class EventsPanel extends ConsumerWidget {
   const EventsPanel({super.key});
   
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final currentSort = ref.watch(selectedSortProvider);
     return Card(
       margin: const EdgeInsets.all(0),
       child: Column(
@@ -25,13 +29,36 @@ class EventsPanel extends ConsumerWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              SecondaryButton(
-                description: 'Show Charts',
-                onPressed: () {
-                  context.push("/charts");
-                },
-                icon: Icons.bar_chart,
-              ),
+              Row(children: [
+                SecondaryButton(
+                  description: 'Show Charts',
+                  onPressed: () {
+                    context.push("/charts");
+                  },
+                  icon: Icons.bar_chart,
+                ),
+                // Filter Button
+                SecondaryButton(
+                  description: 'Filter Events',
+                  icon: Icons.filter_alt,
+                  onPressed: () {
+                    ref.read(filterDrawerProvider.notifier).state =
+                        !ref.watch(filterDrawerProvider);
+                  },
+                ),
+                // Sort Button
+                SecondaryButton(
+                  description: 'Sort $currentSort',
+                  icon: currentSort == 'DESC'
+                      ? Icons.arrow_circle_down
+                      : Icons.arrow_circle_up,
+                  onPressed: () {
+                    // Toggle between ASC and DESC
+                    ref.read(selectedSortProvider.notifier).state =
+                        currentSort == 'ASC' ? 'DESC' : 'ASC';
+                  },
+                ),
+              ]),
               SecondaryButton(
                 description: 'Reset Events',
                 onPressed: () {
